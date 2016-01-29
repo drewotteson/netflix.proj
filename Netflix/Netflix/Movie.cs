f604df04f4d1b73a771ac94354d6b88d07d83942
drewotteson <drew.otteson@yahoo.com>
2016-01-29 12:05:51 -0600

    update file location

---
+++ Netflix/Netflix/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix
+{
+    public class Movie : Title
+    {
+        public float Duration;
+
+        public Movie(float duration, int rating, string name)
+        {
+            this.Duration = duration;
+        }
+    }
+}
---
+++ Netflix/Netflix/Title.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix
+{
+    public class Title
+    {
+        public string name;
+        public int rating;
+
+        public Title(string name, int rating)
+        {
+            this.name = name;
+            this.rating = rating;
+        }
+        public Title()
+        {
+            this.name = null;
+            this.rating = 0;
+        }
+    }
+}
